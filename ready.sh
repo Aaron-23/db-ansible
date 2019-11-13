f8e5721dfda96194465cbc139d10635f2cf933d6
king <50609288+Aaron-23@users.noreply.github.com>
2019-11-14 00:16:26 +0800

    111

--- ready.sh
+++ ready.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-
 yum install -y -q epel-release
 
 yum -y install python2-pip-8.1.2-10.el7.noarch
 
 pip install ansible==2.8.5 -i https://pypi.tuna.tsinghua.edu.cn/simple
-
-
 
 yum -y install install the sshpass program
