03d02c1934df75dd4d5e436d0e4ed99c42959ed8
king <50609288+Aaron-23@users.noreply.github.com>
2019-11-17 22:17:07 +0800

    Update ready.sh

--- ready.sh
+++ ready.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-cat /etc/os-release |grep  CentOS
+cat /etc/os-release |grep  CentOS 
 
 if [ $? -eq 0 ] ;then
+
 echo  "\033[32m Install ansible \033[0m"
-yum install -y -q epel-release && yum -y install python2-pip-8.1.2-10.el7.noarch && yum -y install install the sshpass program && pip install ansible==2.8.5 -i https://pypi.tuna.tsinghua.edu.cn/simple &>/dev/null
+yum install -y -q epel-release && yum -y install python2-pip-8.1.2-10.el7.noarch && yum -y install install the sshpass program && pip install ansible==2.8.5 -i https://pypi.tuna.tsinghua.edu.cn/simple
 
 else
+
 echo  "\033[32m Install ansible \033[0m"
-apt-get update -q && apt-get install -y -q sshpass python-pip  net-tools  && pip install ansible==2.8.5 -i https://pypi.tuna.tsinghua.edu.cn/simple &>/dev/null
+apt-get update -q && apt-get install -y -q sshpass python-pip  net-tools  && pip install ansible==2.8.5 -i https://pypi.tuna.tsinghua.edu.cn/simple 
 
 fi
