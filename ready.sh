3575682bc49f2c44e37db35e34985c3cf3b11793
king <50609288+Aaron-23@users.noreply.github.com>
2019-11-17 20:51:08 +0800

    Update ready.sh

--- ready.sh
+++ ready.sh
@@ -4,12 +4,13 @@ yum install -y -q epel-release
 
 yum -y install python2-pip-8.1.2-10.el7.noarch
 
-pip install ansible==2.8.5 -i https://pypi.tuna.tsinghua.edu.cn/simple
-
 yum -y install install the sshpass program
 
 
 
  apt-get update -q
- apt-get install -y -q sshpass python-pip  net-tools  git
- pip install ansible -i https://pypi.tuna.tsinghua.edu.cn/simple
+ 
+ apt-get install -y -q sshpass python-pip  net-tools 
+ 
+ pip install ansible==2.8.5 -i https://pypi.tuna.tsinghua.edu.cn/simple
+
