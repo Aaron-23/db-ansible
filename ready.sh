a2ce065ae536032158c84a61900cb07520780157
king <50609288+Aaron-23@users.noreply.github.com>
2019-11-17 18:30:43 +0800

    ubuntu

--- ready.sh
+++ ready.sh
@@ -7,3 +7,9 @@ yum -y install python2-pip-8.1.2-10.el7.noarch
 pip install ansible==2.8.5 -i https://pypi.tuna.tsinghua.edu.cn/simple
 
 yum -y install install the sshpass program
+
+
+
+ apt-get update -q
+ apt-get install -y -q sshpass python-pip  net-tools  git
+ pip install ansible -i https://pypi.tuna.tsinghua.edu.cn/simple
